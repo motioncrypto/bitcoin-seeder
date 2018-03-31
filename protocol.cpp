b08b9ef125ecbdeed054872a8ccc13ed4bcabc2a
Omar Cruz <ocruzvar@gmail.com>
2018-03-31 10:43:41 -0600

    Changes for Motion

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xae, 0x7e, 0x5d, 0x74 };
 
 CMessageHeader::CMessageHeader()
 {
